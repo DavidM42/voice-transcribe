7213318b205ef9cef3d998f5659a6755f9e61fab
davidm42 <davidmerz@gmx.de>
2023-07-24 15:52:29 +0200

    utilize cuda in whisper

--- whisper/transcribe.py
+++ whisper/transcribe.py
@@ -1,9 +1,11 @@
 import whisper
 import sys
+import torch
 
 # super simple minimal whisper script to transcribe a file and print out text
 # minimal version of the cli script
 # replace the "small" model with others if you have the gpu/cpu for it (or not and need to use tiny)
-model = whisper.load_model("small")
+device = torch.device('cuda')
+model = whisper.load_model("small", device=device)
 result = model.transcribe(sys.argv[1], fp16=False)
 print(result["text"])
