3740330e764b7245587b7c89ac43c19f634f36f6
davidm42 <davidmerz@gmx.de>
2023-07-24 15:16:11 +0200

    improve bot logic

--- whatsapp/handler.ts
+++ whatsapp/handler.ts
@@ -7,20 +7,23 @@ import { callWhisper } from "../src/logic/call-whisper.js";
 export async function handle(message: Message) {
     if (message.hasMedia) {
         const media = await message.downloadMedia();
-        // media.mimetype = 
-        console.log(media.mimetype);
         if (media.mimetype === 'audio/ogg; codecs=opus') {
+            const chat =  (await message.getChat());
+            chat.sendMessage('Ich transkribiere aktuell...');
             try {
                 const base64Data = media.data.replace(/^data:image\/png;base64,/, "");
                 const fileName = `${randomUUID()}.ogg`;
-
+                
                 await fs.writeFile(`whisper/tmp/${fileName}`, base64Data, 'base64')
                 const transcription = await callWhisper(fileName);
                 await fs.unlink(`whisper/tmp/${fileName}`);
-
-                console.log(transcription);
+                
+                // TODO delete this message after a day or something
+                chat.sendMessage('Anbei das Transkript: \n' + transcription)
             } catch(error: unknown) {
-                (await message.getChat()).sendMessage((error as Error).message);
+                chat.sendMessage((error as Error).message);
+            } finally {
+                message.delete(true)
             }
         }
     }
